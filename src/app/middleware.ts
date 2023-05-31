6acaea9a2d7bfebefe07fc94426295cef88af0e1
Dimitris Klouvas <dimtris.klouvas@gmail.com>
2023-05-31 14:16:40 +0300

    Log headers in middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,6 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+    beforeAuth: ({ headers }) => {
+        console.log({
+            origin: headers.get('origin') || undefined,
+            host: headers.get('host') as string,
+            forwardedPort: headers.get('x-forwarded-port') || undefined,
+            forwardedHost: headers.get('x-forwarded-host') || undefined,
+            referrer: headers.get('referer') || undefined,
+            userAgent: headers.get('user-agent') || undefined,
+        });
+    }
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
