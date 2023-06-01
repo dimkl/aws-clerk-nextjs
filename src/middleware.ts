5db192024ae7fbfdc12955fb0237e8da12e89e42
Dimitris Klouvas <dimtris.klouvas@gmail.com>
2023-06-01 11:44:00 +0300

    minor possible fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export default authMiddleware({
     beforeAuth: ({ headers, nextUrl }) => {
-        console.log(nextUrl.href, '  beforeAuth: ', JSON.stringify({
+        console.log(`${nextUrl.href}  beforeAuth: `, JSON.stringify({
             origin: headers.get('origin'),
             host: headers.get('host'),
             forwardedPort: headers.get('x-forwarded-port'),
@@ -12,9 +12,10 @@ export default authMiddleware({
             referrer: headers.get('referer'),
             userAgent: headers.get('user-agent'),
         }));
+        return NextResponse.next();
     },
     afterAuth: (auth, req) => {
-        console.log(req.nextUrl.href, '  afterAuth: ', JSON.stringify(auth));
+        console.log(`${req.nextUrl.href}  afterAuth: `, JSON.stringify(auth));
         if (!auth.userId && !auth.isPublicRoute) {
             return redirectToSignIn({ returnBackUrl: req.url });
         }
